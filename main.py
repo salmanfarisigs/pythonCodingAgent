aa4f2b23d24ed5709d92776a5eaced55c1c9dc72
Salman Farisi <salmanfarisigs@gmail.com>
2025-12-09 08:10:32 +0530

    updated the agent logic

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
 def main():
-    print("Hello from aiagent!")
+	load_dotenv()
+	api_key = os.environ.get("GEMINI_API_KEY")
+	client = genai.Client(api_key=api_key)
 
+	if len(sys.argv) < 2:
+		print("please enter a valid prompt")
+		sys.exit(1)
+	verbose_flag = False
+	
 
-if __name__ == "__main__":
-    main()
+	if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+		verbose_flag = True
+
+	prompt = sys.argv[1]
+
+	messages = [
+		types.Content(role="user", parts=[types.Part(text=prompt)])
+		]
+	
+	response = client.models.generate_content(
+	model='gemini-2.5-flash', 
+	contents=messages,
+	)
+
+	print(response.text)
+
+	if None or response.usage_metadata is None:
+		print("No usage metadata available.")
+		return
+	if verbose_flag:
+		print(f"user prompt: {prompt}")
+		print(f"Prompt Tokens: {response.usage_metadata.prompt_token_count}")
+		print(f"Response Tokens: {response.usage_metadata.candidates_token_count}")
+main()
