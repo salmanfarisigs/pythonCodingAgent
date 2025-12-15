f1b4b99cff65163486ccca0403497ab3816dea56
Salman Farisi <salmanfarisigs@gmail.com>
2025-12-15 16:52:13 +0530

    added test case and modified get_files_content function

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -13,11 +13,11 @@ def get_file_content(working_directory, file_path):
     
     file_content_string = ""
     try:
-        with open(file_path, "r") as f:
+        with open(abs_file_path, "r") as f:
             file_content_string = f.read(MAX_CHARS)
             if len(file_content_string) >= MAX_CHARS:
                 file_content_string += (
-                    f'[...File "{file_path}" truncated at 10000 characters]'
+                    f'[...File "{abs_file_path}" truncated at 10000 characters]'
                 )
         return file_content_string
     except Exception as e:
--- tests.py
+++ tests.py
@@ -1,12 +1,16 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_content import get_file_content
 def main():
     working_dir = "calculator"
-    root_contents = get_files_info(working_dir)
-    print(root_contents)
-    pkg_contents = get_files_info(working_dir, "pkg")
-    print(pkg_contents)
-    pkg_contents = get_files_info(working_dir, "/bin")
-    print(pkg_contents)
-    pkg_contents = get_files_info(working_dir, "../")
-    print(pkg_contents)
+    print(get_file_content(working_dir,'lorem.txt'))
 main()
+
+
+#from functions.get_files_info import get_files_info
+# root_contents = get_files_info(working_dir)
+    # print(root_contents)
+    # pkg_contents = get_files_info(working_dir, "pkg")
+    # print(pkg_contents)
+    # pkg_contents = get_files_info(working_dir, "/bin")
+    # print(pkg_contents)
+    # pkg_contents = get_files_info(working_dir, "../")
+    # print(pkg_contents)
